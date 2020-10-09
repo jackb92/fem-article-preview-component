c161a55df7836d20b0b483d6f39c29995580cb22
Jack Bentley <jack.bentley@ao.com>
2020-10-09 13:42:42 +0100

    Uncommented some styles

--- function.js
+++ function.js
@@ -1,5 +1,15 @@
 function myFunction() {
+  // var viewPortWidth = window.innerWidth
+
     var x = document.getElementById("myDIV");
+    // var y = document.getElementsByClassName("bottom-section");
+
+    // if (viewPortWidth < 990){
+    //   y.style.display = "none";
+    // } else {
+    //   y.style.display = "block";
+    // }
+
     if (x.style.display === "none") {
       x.style.display = "block";
     } else {
